80457a6eb2769d804ad691b2c828c799efcd8f4a
Ivan Kornasevich <kornasevich@gmail.com>
2025-02-05 09:12:37 +0100

    gas station

--- src/test/scala/Solution.scala
+++ src/test/scala/Solution.scala
@@ -1,35 +1,16 @@
-import java.util
-import scala.annotation.tailrec
-
 object Solution {
-  def productExceptSelf(nums: Array[Int]): Array[Int] = {
-
-    val result = util.Arrays.copyOf(nums, nums.length)
-
-    @tailrec
-    def loop1(idx: Int)(p: Int): Unit = {
-      if (idx < nums.length) {
-        val newP = nums(idx) * p
-        nums.update(idx, newP)
-        loop1(idx + 1)(newP)
-      }
+  def canCompleteCircuit(gas: Array[Int], cost: Array[Int]): Int = {
+    def loop: List[(Int, Int)] => Int = {
+      case Nil => -1
+      case (d, i) :: Nil if d >= 0 => i
+      case _ :: Nil => -1
+      case (0, _) :: xs => loop(xs)
+      case (d1, i1) :: (d2, i2) :: xs if d1 <= 0 && d2 <= 0 => loop((d1 + d2, i1) :: xs)
+      case (d1, i1) :: (d2, i2) :: xs if d1 >= 0 && d2 >= 0 => loop((d1 + d2, i1) :: xs)
+      case (d1, i1) :: (d2, i2) :: xs if d1 >= d2 => loop((d1 + d2, i1) :: xs)
+      case one :: two :: xs => loop(two :: xs ::: List(one))
     }
-    loop1(0)(1)
 
-    def loop2(idx: Int)(p: Int): Unit = {
-      if (idx == result.length - 1) {
-        val newP = p * result(idx)
-        result.update(idx, nums(idx - 1))
-        loop2(idx - 1)(newP)
-      } else if (idx == 0) {
-        result.update(0, p)
-      } else {
-        val newP = p * result(idx)
-        result.update(idx, nums(idx - 1) * p)
-        loop2(idx - 1)(newP)
-      }
-    }
-    loop2(result.length - 1)(1)
-    result
+    loop((gas zip cost).map { case (gas, cost) => gas - cost }.zipWithIndex.toList)
   }
 }
--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -4,14 +4,14 @@ import org.scalatest.wordspec.AnyWordSpec
 final class Test extends AnyWordSpec with Matchers {
 
   "simple" in {
-    Solution.productExceptSelf(Array(1, 2, 3)) shouldBe Array(6, 3, 2)
+    println{
+      Solution.canCompleteCircuit(Array(1, 2, 3, 4, 5), Array(3, 4, 5, 1, 2))
+    }
   }
 
   "two" in {
-    Solution.productExceptSelf(Array(5, 10)) shouldBe Array(10, 5)
   }
 
   "fail 1" in {
-    Solution.productExceptSelf(Array(2, 3, 5, 0)) shouldBe Array(0, 0, 0, 30)
   }
 }
