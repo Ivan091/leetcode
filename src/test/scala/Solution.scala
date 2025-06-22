aa05ce42d2e17ade0f7c38b7962f514d9def25df
Ivan Kornasevich <kornasevich@gmail.com>
2025-06-22 21:34:20 +0200

    test

--- src/test/scala/Solution.scala
+++ src/test/scala/Solution.scala
@@ -1,23 +1,23 @@
 object Solution {
-	def canConstruct(ransomNote: String, magazine: String): Boolean = {
+  def canConstruct(ransomNote: String, magazine: String): Boolean = {
 
-		def loop(map: Map[Char, Int]): List[Char] => Boolean = {
-			case x :: xs =>
-				if (map.contains(x)) {
-					val newMap =
-						map.updatedWith(x) {
-							case Some(cnt) if cnt <= 1 => Option.empty
-							case Some(cnt) => Option(cnt - 1)
-							case None => None
-						}
-					loop(newMap)(xs)
-				} else {
-					false
-				}
-			case Nil => true
-		}
+    def loop(map: Map[Char, Int]): List[Char] => Boolean = {
+      case x :: xs =>
+        if (map.contains(x)) {
+          val newMap =
+            map.updatedWith(x) {
+              case Some(cnt) if cnt <= 1 => Option.empty
+              case Some(cnt)             => Option(cnt - 1)
+              case None                  => None
+            }
+          loop(newMap)(xs)
+        } else {
+          false
+        }
+      case Nil => true
+    }
 
-		val magazineMap = magazine.toVector.groupBy(identity).map { case (k, v) => (k, v.size) }
-		loop(magazineMap)(ransomNote.toList)
-	}
+    val magazineMap = magazine.toVector.groupBy(identity).map { case (k, v) => (k, v.size) }
+    loop(magazineMap)(ransomNote.toList)
+  }
 }
